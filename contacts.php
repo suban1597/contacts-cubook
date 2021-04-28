cfbdd2537496c0e72623fd81c7027de36de56dc1
suban1597 <59216475+suban1597@users.noreply.github.com>
2021-04-28 15:06:08 +0700

    Update contacts.php

--- contacts.php
+++ contacts.php
@@ -21,7 +21,7 @@ if($name<>"" || $email <> "" || $mesg <> "") {
 $res = notify_message($message,$stickerPkg,$stickerId,$token);
  echo "<center>ส่งข้อความเรียบร้อยแล้ว</center>";
  ?>
-<meta http-equiv="refresh" content="2;url=request.html">
+<meta http-equiv="refresh" content="2;url=contacts.html">
 <?php
 
 } else {
